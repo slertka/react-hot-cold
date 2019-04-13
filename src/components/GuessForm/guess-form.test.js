664cd1b934b493914ede79866e015a86e948c7f5
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-04-13 12:12:29 -0700

    GuessForm callback tests

--- src/components/GuessForm/guess-form.test.js
+++ src/components/GuessForm/guess-form.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { configure, shallow } from "enzyme";
+import { configure, shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
@@ -9,5 +9,14 @@ import GuessForm from "./guess-form";
 describe("<GuessForm />", () => {
   it("should render without crashing", () => {
     shallow(<GuessForm />);
+  });
+
+  it("should call onMakeGuess when form is submit", () => {
+    const callback = jest.fn();
+    const wrapper = mount(<GuessForm onMakeGuess={callback} />);
+    const value = "1";
+    wrapper.find('input[type="number"]').instance().value = value;
+    wrapper.simulate("submit");
+    expect(callback).toHaveBeenCalledWith(value);
   });
 });
