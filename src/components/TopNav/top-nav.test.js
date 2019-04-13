b12322eba47c7b0d3fb17493da9a27f53cc098bb
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-04-13 11:56:17 -0700

    TopNav callback tests

--- src/components/TopNav/top-nav.test.js
+++ src/components/TopNav/top-nav.test.js
@@ -10,4 +10,20 @@ describe("<TopNav />", () => {
   it("renders without crashing", () => {
     shallow(<TopNav />);
   });
+
+  it("should call onRestartGame when new game is clicked", () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    const link = wrapper.find(".new");
+    link.simulate("click", { preventDefault() {} });
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("should call onGenerateAuralUpdate when status-link is clicked", () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+    const link = wrapper.find(".status-link");
+    link.simulate("click", { preventDefault() {} });
+    expect(callback).toHaveBeenCalled();
+  });
 });
